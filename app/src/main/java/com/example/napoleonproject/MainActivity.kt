21a472f0fc23ac4b8e5c4687c92a338d615077b9
Streeeeck <52788224+Streeeeck@users.noreply.github.com>
2020-12-17 03:28:48 +0300

    Revert "second activity"
    This reverts commit 25d700cb

--- app/src/main/java/com/example/napoleonproject/MainActivity.kt
+++ app/src/main/java/com/example/napoleonproject/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.napoleonproject
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     var news = News()
-    private val TAG = "MainActivity"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,68 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         news.Testinit()
 
-        ETList.text = Editable.Factory.getInstance().newEditable(news.toString())
+        etm.text = Editable.Factory.getInstance().newEditable(news.toString())
 
 
-    }
-    fun openLastNews(view : View){
-        val intent = Intent(this, LastNewsActivity::class.java)
-        intent.putExtra("oneN", news.lastNews())
-        startActivityForResult(intent, 1);
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == RESULT_OK) {
-            var t = news.searchByString(data?.getSerializableExtra("id") as OneNews)
-            news.arrayOfNews[t].checked = true
-            ETList.text = Editable.Factory.getInstance().newEditable(news.toString())
-
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause")
     }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Log.d(TAG, "onSaveInstanceState")
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        Log.d(TAG, "onRestoreInstanceState")
-    }
-
-
-
 
 
 
--- app/src/main/java/com/example/napoleonproject/OneNews.kt
+++ app/src/main/java/com/example/napoleonproject/OneNews.kt
@@ -3,18 +3,9 @@ package com.example.napoleonproject
 import java.io.Serializable
 
 
-data class OneNews(
-    val date: String,
-    val res: String,
-    val head: String,
-    val text: String,
-    var checked : Boolean = false
-
-) : Serializable
-{
+data class OneNews(val date: String, val res: String, val head: String, val text: String){
 
     override fun toString(): String {
-        return if (checked) "$date $res $head $checked"
-        else "$date $res $head "
+        return "$date $res $head"
     }
-}
+} //:Serializable
